4abcd6989a6c6431f6e8c624945d5aa1bc9a5bd2
Galina.Rubo <Rubo_eh74@itstepru.onmicrosoft.com>
2022-12-18 22:12:48 +0700

    реализованы проверки на ошибочный ввод данных

--- ExamTicketsAppWPF/ExamTicketsAppWPF/ViewModels/CommandViewModel.cs
+++ ExamTicketsAppWPF/ExamTicketsAppWPF/ViewModels/CommandViewModel.cs
@@ -75,118 +75,124 @@ namespace ExamTicketsAppWPF.ViewModels
 			{
 				return madeCommand = new Command((o) =>
 				{
-					using (TicketsDbContext db = new TicketsDbContext())
+					if (Sb != null && Cb != null)
 					{
-						db.Database.EnsureCreated();
-						var indexc = (db.categories.First(c => c.CategoryName == Cb)).Id;
-						var indexs = (db.subjects.First(s => s.SubjectName == Sb)).Id;
-						var quest = db.questions.Where(x => x.IdSubject == indexs && x.IdCategory == indexc).ToList();
-						var prac = db.practices.Where(a => a.idsubject == indexs && a.idcategory == indexc).ToList();
-						int kb = 1;
-						int kbr = 0;
-						string path = @"E:\SQL\ExamTickets.docx";
-						var doc = Xceed.Words.NET.DocX.Create(path);
-						if (quest.Count == 0) MessageBox.Show("Вопросов с таким предметом и категорией в базе данных нет");
-						else
+						using (TicketsDbContext db = new TicketsDbContext())
 						{
-							kbr = quest.Count - Convert.ToInt32(Kb) * Convert.ToInt32(Kq);
-							if (kbr < 0)
+							db.Database.EnsureCreated();
+							var indexc = (db.categories.First(c => c.CategoryName == Cb)).Id;
+							var indexs = (db.subjects.First(s => s.SubjectName == Sb)).Id;
+							var quest = db.questions.Where(x => x.IdSubject == indexs && x.IdCategory == indexc).ToList();
+							var prac = db.practices.Where(a => a.idsubject == indexs && a.idcategory == indexc).ToList();
+							int kb = 1;
+							int kbr = 0;
+							string path = @"E:\SQL\ExamTickets.docx";
+							var doc = Xceed.Words.NET.DocX.Create(path);
+							if (quest.Count == 0) MessageBox.Show("Вопросов с таким предметом и категорией в базе данных нет");
+							else
 							{
-								Kb = (quest.Count / Convert.ToInt32(Kq)).ToString();
-								Console.Write("Количество билетов может быть только " + Kb + "\n");
-							}
-						};
-						if (prac.Count == 0) MessageBox.Show("Задач с таким предметом и категорией в базе данных нет");
-						else
-						{
-							kbr = prac.Count - Convert.ToInt32(Kb) * Convert.ToInt32(Kt);
-							if (kbr < 0)
-							{
-								Kb = (prac.Count / Convert.ToInt32(Kt)).ToString();
-								Console.Write("Количество билетов может быть только " + Kb + "\n");
-							}
-						};
-
-						while (kb <= Convert.ToInt32(Kb))
-						{
-							Paragraph headline = doc.InsertParagraph($"Билет № {kb}" + "\n\n");
-							headline.Color(System.Drawing.Color.Red);
-							headline.Bold();
-
-							if (Kq != "0")
-							{
-
-								int i = 0;
-								List<string> dellistq = new List<string>();
-								var tasksq = new Task[quest.Count()];
-								foreach (var q in quest)
+								kbr = quest.Count - Convert.ToInt32(Kb) * Convert.ToInt32(Kq);
+								if (kbr < 0)
 								{
-									tasksq[i] = Task.Run(() =>
-									{
-										Paragraph headlineq = doc.InsertParagraph($"Вопрос № {i + 1}");
-										headlineq.Color(System.Drawing.Color.Gray);
-										headlineq.Bold();
-										if (q.FormatQuestion == "TXT") doc.InsertParagraph("\n" + q.ContentQuestion + "\n");
-										else
-										{
-											Image image = doc.AddImage(q.ContentQuestion);
-											Picture picture = image.CreatePicture();
-											Paragraph p1 = doc.InsertParagraph();
-											p1.AppendPicture(picture);
-										}
-									});
-									tasksq[i].Wait();
-									dellistq.Add(q.ContentQuestion);
-									if (++i == Convert.ToInt32(Kq)) break;
+									Kb = (quest.Count / Convert.ToInt32(Kq)).ToString();
+									Console.Write("Количество билетов может быть только " + Kb + "\n");
 								}
-								foreach (var del in dellistq)
+							};
+							if (prac.Count == 0) MessageBox.Show("Задач с таким предметом и категорией в базе данных нет");
+							else
+							{
+								kbr = prac.Count - Convert.ToInt32(Kb) * Convert.ToInt32(Kt);
+								if (kbr < 0)
 								{
-									quest.RemoveAll(x => x.ContentQuestion == del);
-
+									Kb = (prac.Count / Convert.ToInt32(Kt)).ToString();
+									Console.Write("Количество билетов может быть только " + Kb + "\n");
 								}
 							};
 
-
-
-							if (Kt != "0")
+							while (kb <= Convert.ToInt32(Kb))
 							{
+								Paragraph headline = doc.InsertParagraph($"Билет № {kb}" + "\n\n");
+								headline.Color(System.Drawing.Color.Red);
+								headline.Bold();
 
-								int i = 0;
-								var tasksp = new Task[prac.Count()];
-								List<string> dellistp = new List<string>();
-								foreach (var p in prac)
+								if (Kq != "0")
 								{
-									tasksp[i] = Task.Run(() =>
+
+									int i = 0;
+									List<string> dellistq = new List<string>();
+									var tasksq = new Task[quest.Count()];
+									foreach (var q in quest)
 									{
-										Paragraph headlinep = doc.InsertParagraph($"Задача № {i + 1}");
-										headlinep.Color(System.Drawing.Color.Gray);
-										headlinep.Bold();
-										if (p.FormatPractice == "TXT") doc.InsertParagraph("\n" + p.ContentPractice + "\n");
-										else
+										tasksq[i] = Task.Run(() =>
 										{
-											Image image = doc.AddImage(p.ContentPractice);
-											Picture picture = image.CreatePicture();
-											Paragraph p1 = doc.InsertParagraph();
-											p1.AppendPicture(picture);
-										}
-									});
-									tasksp[i].Wait();
-									dellistp.Add(p.ContentPractice);
-									if (++i == Convert.ToInt32(Kt)) break;
-								}
-								foreach (var del in dellistp)
-								{
-									prac.RemoveAll(x => x.ContentPractice == del);
-								}
-							}
-							doc.InsertParagraph($"--------------------------------------------------------------------------------------------------------------------------------\f");
-							kb++;
-						}
+											Paragraph headlineq = doc.InsertParagraph($"Вопрос № {i + 1}");
+											headlineq.Color(System.Drawing.Color.Gray);
+											headlineq.Bold();
+											if (q.FormatQuestion == "TXT") doc.InsertParagraph("\n" + q.ContentQuestion + "\n");
+											else
+											{
+												Image image = doc.AddImage(q.ContentQuestion);
+												Picture picture = image.CreatePicture();
+												Paragraph p1 = doc.InsertParagraph();
+												p1.AppendPicture(picture);
+											}
+										});
+										tasksq[i].Wait();
+										dellistq.Add(q.ContentQuestion);
+										if (++i == Convert.ToInt32(Kq)) break;
+									}
+									foreach (var del in dellistq)
+									{
+										quest.RemoveAll(x => x.ContentQuestion == del);
 
-						doc.Save();
+									}
+								};
+
+
+
+								if (Kt != "0")
+								{
+
+									int i = 0;
+									var tasksp = new Task[prac.Count()];
+									List<string> dellistp = new List<string>();
+									foreach (var p in prac)
+									{
+										tasksp[i] = Task.Run(() =>
+										{
+											Paragraph headlinep = doc.InsertParagraph($"Задача № {i + 1}");
+											headlinep.Color(System.Drawing.Color.Gray);
+											headlinep.Bold();
+											if (p.FormatPractice == "TXT") doc.InsertParagraph("\n" + p.ContentPractice + "\n");
+											else
+											{
+												Image image = doc.AddImage(p.ContentPractice);
+												Picture picture = image.CreatePicture();
+												Paragraph p1 = doc.InsertParagraph();
+												p1.AppendPicture(picture);
+											}
+										});
+										tasksp[i].Wait();
+										dellistp.Add(p.ContentPractice);
+										if (++i == Convert.ToInt32(Kt)) break;
+									}
+									foreach (var del in dellistp)
+									{
+										prac.RemoveAll(x => x.ContentPractice == del);
+									}
+								}
+								doc.InsertParagraph($"--------------------------------------------------------------------------------------------------------------------------------\f");
+								kb++;
+							}
+
+							doc.Save();
+						}
+						if (Kb != "0")
+							MessageBox.Show("Билеты сформированы");
+						return;
 					}
-					if (Kb != "0")
-						MessageBox.Show("Билеты сформированы");
+					if (Sb == null) MessageBox.Show("Введите предмет билетов");
+					if (Cb == null) MessageBox.Show("Введите категорию билетов");
 					return;
 				});
 			}
